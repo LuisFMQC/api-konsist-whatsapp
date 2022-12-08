91dace7e362c4e278df9264489dd81049aeaa621
Luis Fernando <luismqdf@gmail.com>
2022-12-08 15:56:04 -0300

    envio e leitura de mensagens funcionando

---
+++ app3.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const https = require('https');
+require('dotenv').config();
+
+const options = {
+  key: fs.readFileSync('./SSL/code.key'),
+  cert: fs.readFileSync('./SSL/code.crt'),
+};
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+app.listen(PORT || 1337, () => console.log('Api rodando na porta ' + PORT));
+// Cria a instância do server e escuta na porta 3000
+https
+  .createServer(options, (req, res) => {
+    res.writeHead(200);
+    res.end('Hello world using HTTPS!\n');
+  })
+  .listen(80);
